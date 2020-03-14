c2bce742f399cb5d8037cdfe4682624fd456c761
Caleb <calebegg@google.com>
2020-03-14 09:14:39 -0400

    Editor for rules (not hooked up)

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Automaton, State } from './types';
+import { Automaton, State, Rule, Query } from './types';
 import { Action } from './action';
 
 interface EditorProps {
@@ -8,16 +8,27 @@ interface EditorProps {
   dispatch: React.Dispatch<Action>;
 }
 
-export function Editor({ automaton: { states }, dispatch }: EditorProps) {
+export function Editor({
+  automaton: { states, rules },
+  dispatch,
+}: EditorProps) {
   return (
-    <details className="root">
+    <details>
       <summary></summary>
-      <details open>
-        <summary>States</summary>
-        {states.map((state, i) => (
-          <StateEditor key={i} id={i} state={state} dispatch={dispatch} />
-        ))}
-      </details>
+      <h2>States</h2>
+      {states.map((state, i) => (
+        <StateEditor key={i} id={i} state={state} dispatch={dispatch} />
+      ))}
+      <h2>Rules</h2>
+      {rules.map((rule, i) => (
+        <RuleEditor
+          key={i}
+          id={i}
+          rule={rule}
+          states={states}
+          dispatch={dispatch}
+        />
+      ))}
     </details>
   );
 }
@@ -30,7 +41,7 @@ interface StateEditorProps {
 
 export function StateEditor({ state, id, dispatch }: StateEditorProps) {
   return (
-    <div>
+    <div className="state-editor">
       <input
         type="text"
         value={state.name}
@@ -46,5 +57,69 @@ export function StateEditor({ state, id, dispatch }: StateEditorProps) {
         }}
       ></input>
     </div>
+  );
+}
+
+interface RuleEditorProps {
+  rule: Rule;
+  states: readonly State[];
+  id: number;
+  dispatch: React.Dispatch<Action>;
+}
+
+export function RuleEditor({ rule, states, id, dispatch }: RuleEditorProps) {
+  return (
+    <div className="rule-editor">
+      <div className="before">
+        <div className="row">
+          <StateSelect states={states} value={rule.before[0][0]} />
+          <StateSelect states={states} value={rule.before[0][1]} />
+        </div>
+        <div className="row">
+          <StateSelect states={states} value={rule.before[1][0]} />
+          <StateSelect states={states} value={rule.before[1][1]} />
+        </div>
+      </div>
+      <div>=></div>
+      {rule.after.map((a, i) => (
+        <div key={i} className="after">
+          <div className="row">
+            <StateSelect states={states} value={a.result[0][0]} />
+            <StateSelect states={states} value={a.result[0][1]} />
+          </div>
+          <div className="row">
+            <StateSelect states={states} value={a.result[1][0]} />
+            <StateSelect states={states} value={a.result[1][1]} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+interface StateSelectProps {
+  states: readonly State[];
+  value: Query;
+  onChange?: (value: number) => void;
+}
+
+export function StateSelect({ states, value, onChange }: StateSelectProps) {
+  return (
+    <select
+      value={JSON.stringify(value)}
+      style={{ backgroundColor: states.find(s => s.id === value.id)?.color }}
+    >
+      <option value="{all: true}">*</option>
+      {states.map(s => (
+        <option key={s.id} value={JSON.stringify({ id: s.id })}>
+          {s.name}
+        </option>
+      ))}
+      {states.map((s, id) => (
+        <option key={s.id} value={JSON.stringify({ id: s.id, negate: true })}>
+          ^{s.name}
+        </option>
+      ))}
+    </select>
   );
 }
--- src/types.ts
+++ src/types.ts
@@ -13,19 +13,14 @@ export type State = Immutable<{
 
 export type Block = Immutable<[[Query, Query], [Query, Query]]>;
 
-export type Query = Immutable<
-  | {
-      /** Matches cells with this ID (but see below) */
-      id?: number;
-      /** Matches cells that *don't* match ID */
-      negate?: boolean;
-      all?: false;
-    }
-  | {
-      /** Matches all cells. Other fields are ignored if this is specified. */
-      all: true;
-    }
->;
+export type Query = Immutable<{
+  /** Matches cells with this ID (but see below) */
+  id?: number;
+  /** Matches cells that *don't* match ID */
+  negate?: true;
+  /** Matches all cells. Other fields are ignored if this is specified. */
+  all?: true;
+}>;
 
 export type Rule = Immutable<{
   before: Block;
